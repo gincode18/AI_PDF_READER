90f7fce6b1a6e5f737bc3bdfaab5ca5de38c0e8d
gincode18 <vishalkamboj9211@gmail.com>
2023-12-10 15:55:22 +0530

    fixed build error   :)

--- src/components/MarginWrapper.tsx
+++ src/components/MarginWrapper.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/Utils"
+import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
 function MarginWrapper({className, children }: { className?:string,children: ReactNode}) {
