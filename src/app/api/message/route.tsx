edd0d367ebce01663e93842e97ce9f151b11ec7e
gincode18 <vishalkamboj9211@gmail.com>
2023-12-10 12:37:20 +0530

    Holy Cow it works how i dont even care Suuuuuuuuuuuuuu :)

--- src/app/api/message/route.tsx
+++ src/app/api/message/route.tsx
@@ -1,39 +1,36 @@
-import { db } from '@/db'
-import { openai } from '@/lib/openai'
-import { getPineconeClient } from '@/lib/pinecone'
-import { SendMessageValidator } from '@/lib/validators/SendMessageValidator'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { NextRequest } from 'next/server'
+import { db } from '@/db';
+import { openai } from '@/lib/openai';
+import { pinecone } from '@/lib/pinecone';
+import { SendMessageValidator } from '@/lib/validators/SendMessageValidator';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { NextRequest } from 'next/server';
 
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 export const POST = async (req: NextRequest) => {
   // endpoint for asking a question to a pdf file
 
-  const body = await req.json()
+  const body = await req.json();
 
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-  const { id: userId } = user
+  const { id: userId } = user;
 
-  if (!userId)
-    return new Response('Unauthorized', { status: 401 })
+  if (!userId) return new Response('Unauthorized', { status: 401 });
 
-  const { fileId, message } =
-    SendMessageValidator.parse(body)
+  const { fileId, message } = SendMessageValidator.parse(body);
 
   const file = await db.file.findFirst({
     where: {
       id: fileId,
       userId,
     },
-  })
+  });
 
-  if (!file)
-    return new Response('Not found', { status: 404 })
+  if (!file) return new Response('Not found', { status: 404 });
 
   await db.message.create({
     data: {
@@ -42,28 +39,20 @@ export const POST = async (req: NextRequest) => {
       userId,
       fileId,
     },
-  })
+  });
 
   // 1: vectorize message
+  const pineconeIndex = pinecone.Index('quill');
+
   const embeddings = new OpenAIEmbeddings({
     openAIApiKey: process.env.OPENAI_API_KEY,
-  })
+  });
 
-  const pinecone = await getPineconeClient()
-  const pineconeIndex = pinecone.Index('quill')
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+  });
 
-  const vectorStore = await PineconeStore.fromExistingIndex(
-    embeddings,
-    {
-      pineconeIndex,
-      namespace: file.id,
-    }
-  )
-
-  const results = await vectorStore.similaritySearch(
-    message,
-    4
-  )
+  const results = await vectorStore.similaritySearch(message, 4);
 
   const prevMessages = await db.message.findMany({
     where: {
@@ -73,14 +62,12 @@ export const POST = async (req: NextRequest) => {
       createdAt: 'asc',
     },
     take: 6,
-  })
+  });
 
   const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage
-      ? ('user' as const)
-      : ('assistant' as const),
+    role: msg.isUserMessage ? ('user' as const) : ('assistant' as const),
     content: msg.text,
-  }))
+  }));
 
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
@@ -100,9 +87,8 @@ export const POST = async (req: NextRequest) => {
   
   PREVIOUS CONVERSATION:
   ${formattedPrevMessages.map((message) => {
-    if (message.role === 'user')
-      return `User: ${message.content}\n`
-    return `Assistant: ${message.content}\n`
+    if (message.role === 'user') return `User: ${message.content}\n`;
+    return `Assistant: ${message.content}\n`;
   })}
   
   \n----------------\n
@@ -113,7 +99,7 @@ export const POST = async (req: NextRequest) => {
   USER INPUT: ${message}`,
       },
     ],
-  })
+  });
 
   const stream = OpenAIStream(response, {
     async onCompletion(completion) {
@@ -124,9 +110,9 @@ export const POST = async (req: NextRequest) => {
           fileId,
           userId,
         },
-      })
+      });
     },
-  })
+  });
 
-  return new StreamingTextResponse(stream)
-}
+  return new StreamingTextResponse(stream);
+};
--- src/app/api/uploadthing/core.tsx
+++ src/app/api/uploadthing/core.tsx
@@ -1,6 +1,10 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { pinecone } from "@/lib/pinecone";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 const f = createUploadthing();
 
@@ -9,7 +13,7 @@ const middleware = async () => {
   const user = getUser();
 
   if (!user || !user.id) throw new Error("Unauthorized");
-console.log("Auth ===========================Middle");
+  console.log("Auth ===========================Middle");
 
   return { userId: user.id };
 };
@@ -44,12 +48,38 @@ const onUploadComplete = async ({
   });
 
   try {
-    console.log("Uploading");
-    
+    console.log("====================================");
+    console.log("fetchibn blob");
+    console.log("====================================");
+
     const response = await fetch(
       `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
     );
+    const blob = await response.blob();
 
+    const loader = new PDFLoader(blob);
+
+    const pageLevelDocs = await loader.load();
+
+    const pagesAmt = pageLevelDocs.length;
+    console.log("====================================");
+    console.log("we are in pine cone");
+    console.log("====================================");
+    // vectorize and index entire document
+    const pineconeIndex = pinecone.Index("quill");
+    console.log("====================================");
+    console.log("index Generated");
+    console.log("====================================");
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+
+    await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+      pineconeIndex,
+    });
+    console.log("====================================");
+    console.log("Dociment Stored");
+    console.log("====================================");
     await db.file.update({
       data: {
         uploadStatus: "SUCCESS",
